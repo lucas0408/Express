867296e481553d99cbec39db871259d0109d7be9
lucas0408 <lucasgabrielnavassabino@gmail.com>
2025-02-23 23:21:00 -0300

    adding html and server

--- hello-word.ts
+++ hello-word.ts
@@ -1,3 +1,20 @@
-import {Router} from 'express'
+import express, { Request, Response } from 'express';
+import { config } from 'dotenv'
+import path from 'path'
+import { readFileSync } from 'fs'
 
-const app = Router()
+config()
+const app = express();
+app.set(express.static(''))
+const url = process.env.API_BASE_URL ?? 'http://localhost'
+const port = process.env.API_PORT ?? 3300
+
+app.get('/', (req: Request, res: Response) => {
+    const homepagePath = path.join(process.cwd(), 'home.html')
+    const homePage = readFileSync(homepagePath)
+    return res.status(200).send(homePage);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${url}:${port}`);
+});
