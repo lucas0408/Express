9590cfbf44b510033a56fb7ebd7b52c3b04189ea
lucas0408 <lucasgabrielnavassabino@gmail.com>
2025-02-23 23:21:35 -0300

    adding html and server

--- hello-word.ts
+++ hello-word.ts
@@ -5,14 +5,28 @@ import { readFileSync } from 'fs'
 
 config()
 const app = express();
-app.set(express.static(''))
+app.use(express.static(path.join(__dirname, 'public')))
 const url = process.env.API_BASE_URL ?? 'http://localhost'
 const port = process.env.API_PORT ?? 3300
+const users = [
+    {
+        name: 'fulano',
+        idade: 34
+    },
+    {
+        name: 'ciclano',
+        idade: 23
+    }
+]
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/api', (req: Request, res: Response) => {
     const homepagePath = path.join(process.cwd(), 'home.html')
     const homePage = readFileSync(homepagePath)
     return res.status(200).send(homePage);
+});
+
+app.get('/api/users', (req: Request, res: Response) => {
+    return res.json(users)
 });
 
 app.listen(port, () => {
